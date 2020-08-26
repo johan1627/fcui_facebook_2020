877b1ced56d2b9948d49bd5385892cba4cdd8e38
johan1627 <johanns.sandida@gmail.com>
2020-08-26 21:17:15 +0700

    image

---
+++ lib/misc/constant.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+// Text Colors
+Color primaryColor = Color(0xFF4267B2);
+Color greyDark = Color(0xFF898F9C);
+Color greyLight = Color(0xFFf7f7f7);
+
+Color greenColor = Color(0xFF2E7D32);
+
+Color whiteColor = Color(0xFFEFEFEF);
+Color blackColor = Color(0xFF262626);
+
+// Text Styles
+TextStyle h1 = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 20.0,
+);
+
+TextStyle h2 = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 18.0,
+);
+
+TextStyle h3 = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 16.0,
+);
+
+TextStyle lableFont = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 14.0,
+);
+
+TextStyle footFont = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 12.0,
+);
+
+// Icons
+String iconSearch = "assets/icons/icon_search.svg";
+String iconMessenger = "assets/icons/icon_messenger.svg";
+
+String iconHome = "assets/icons/icon_home.svg";
+String iconVideo = "assets/icons/icon_video.svg";
+String iconAccount = "assets/icons/icon_account.svg";
+String iconMotification = "assets/icons/icon_notification.svg";
+String iconMenu = "assets/icons/icon_menu.svg";
